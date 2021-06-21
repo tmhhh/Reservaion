229adf142f040e38f03692cc858903f0cd7b4315
khanghoang <khanghoang1702@gmail.com>
2021-06-22 01:14:25 +0700

    emkhangbenmaster

--- views/public/js/homepage.js
+++ views/public/js/homepage.js
@@ -6,8 +6,21 @@
 //         // //     type: 'Get',
 //         // // }).then((data)=>{
 //         // //     console.log(data);
-//         // // }); 
+//         // // });
 // alert('2');
 //         window.location.replace (`http://localhost:3000/search/byCate?cate=${index+1}`);
 //     })
 // });
+const readmoreBtn = document.querySelector(".btn-readmore");
+const hiddenNews = document.querySelectorAll(".news_hidden");
+readmoreBtn = document.addEventListener("click", (e) => {
+  console.log("Aaaaaaaaaaa");
+  hiddenNews.forEach((news) => {
+    news.classList.toggle("news-show");
+    if (news.classList.contains("news-show")) {
+      readmoreBtn.innerHTML = `<i class="fa fa-caret-up"></i>`;
+    } else {
+      readmoreBtn.innerHTML = `<i class="fa fa-caret-down"></i>`;
+    }
+  });
+});
